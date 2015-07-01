923b8f90b4abca4344d2323886c8694fdde65c0d
Michael Klopf <klopf.michael@gmail.com>
2015-07-01 23:29:32 +0200

    Finished first lesson.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,7 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber: Bool = false
 
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            display.text! = display.text! + digit
+        } else {
+            display.text! = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
