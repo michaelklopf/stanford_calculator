faf0a1ec304a208ac2552bee0b17cbabb51367a5
Michael Klopf <klopf.michael@gmail.com>
2015-07-02 09:06:59 +0200

    Multiply with closure function

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,15 +11,60 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var display: UILabel!
     
-    var userIsInTheMiddleOfTypingANumber: Bool = false
+    var userIsInTheMiddleOfTypingANumber = false
+    
+    var operandStack = Array<Double>()
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
 
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
         if userIsInTheMiddleOfTypingANumber {
-            display.text! = display.text! + digit
+            display.text = display.text! + digit
         } else {
-            display.text! = digit
+            display.text = digit
             userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+        print("operandStack is \(operandStack)")
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        switch operation {
+        case "x":
+            performOperation({(op1: Double, op2: Double) -> Double in
+                return op1 * op2;
+            })
+//        case "÷":
+//            performOperation(divide)
+//        case "+":
+//            performOperation(add)
+//        case "-":
+//            performOperation(substract)
+        default: break
+        }
+    }
+    
+    func performOperation(operation: (Double, Double) -> Double) { // ... is a function type
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
         }
     }
     
